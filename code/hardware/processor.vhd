ec2a9cd82a33dcc4281574293a8c200fdd57f1f8
Odd <oddweb42+gith@gmail.com>
2013-09-23 21:06:06 +0200

    figured out how to use, uh, variables? how to use N and M in component declaration in processor.vhd

--- code/hardware/processor.vhd
+++ code/hardware/processor.vhd
@@ -37,13 +37,13 @@ entity processor is
 		
     Port (
 		clk : in STD_LOGIC;
-		reset					: in STD_LOGIC;
+		reset               : in STD_LOGIC;
 		processor_enable	: in  STD_LOGIC;
 		imem_address 		: out  STD_LOGIC_VECTOR (MEM_ADDR_BUS-1 downto 0);
 		imem_data_in 		: in  STD_LOGIC_VECTOR (MEM_DATA_BUS-1 downto 0);
 		dmem_data_in 		: in  STD_LOGIC_VECTOR (MEM_DATA_BUS-1 downto 0);
 		dmem_address 		: out  STD_LOGIC_VECTOR (MEM_ADDR_BUS-1 downto 0);
-		dmem_address_wr	: out  STD_LOGIC_VECTOR (MEM_ADDR_BUS-1 downto 0);
+		dmem_address_wr	    : out  STD_LOGIC_VECTOR (MEM_ADDR_BUS-1 downto 0);
 		dmem_data_out		: out  STD_LOGIC_VECTOR (MEM_DATA_BUS-1 downto 0);
 		dmem_write_enable	: out  STD_LOGIC
 	);
@@ -56,12 +56,12 @@ architecture Behavioral of processor is
 	-- Math, yo. Probably.
 	component alu is
 		generic (
-			N: natural := 32
+			N: integer := MEM_DATA_BUS
 		);
 		port (
 		X			: in STD_LOGIC_VECTOR(N-1 downto 0);
 		Y			: in STD_LOGIC_VECTOR(N-1 downto 0);
-		ALU_IN	: in ALU_INPUT;
+		ALU_IN	    : in ALU_INPUT;
 		R			: out STD_LOGIC_VECTOR(N-1 downto 0);
 		FLAGS		: out ALU_FLAGS
 		);
@@ -70,47 +70,59 @@ architecture Behavioral of processor is
 	-- we need adders to increment the PC
 	component adder is
 		generic (
-			N: natural := 32
+			N: integer := MEM_ADDR_BUS
 		);
 		port (
-			X	: in	STD_LOGIC_VECTOR(N-1 downto 0);
-			Y	: in	STD_LOGIC_VECTOR(N-1 downto 0);
-			CIN	: in	STD_LOGIC;
+			X   	: in	STD_LOGIC_VECTOR(N-1 downto 0);
+			Y   	: in	STD_LOGIC_VECTOR(N-1 downto 0);
+			CIN	    : in	STD_LOGIC;
 			COUT	: out	STD_LOGIC;
-			R	: out	STD_LOGIC_VECTOR(N-1 downto 0)
+			R   	: out	STD_LOGIC_VECTOR(N-1 downto 0)
 		);
 	end component;
 	
+    component PC is 
+        port (
+            CLK     : in STD_LOGIC;
+            pc_in   : in  STD_LOGIC_VECTOR (N-1 downto 0);
+            pc_out  : out  STD_LOGIC_VECTOR (N-1 downto 0)
+        );
+    end component;
+    
     -- memory component for the instruction and data memory blocks
 	component memory is 
 		generic (
-			N: natural := 32;
-			M: natural := 32
+			N: natural := MEM_DATA_BUS;
+			M: natural := MEM_ADDR_BUS
 		);
 		port (
-			CLK			: in STD_LOGIC;
+			CLK		    	:   in STD_LOGIC;
 			RESET			:	in  STD_LOGIC;	
-			W_ADDR		:	in  STD_LOGIC_VECTOR (N-1 downto 0);	-- Address to write data
-			WRITE_DATA	:	in  STD_LOGIC_VECTOR (N-1 downto 0);	-- Data to be written
-			MemWrite		:	in  STD_LOGIC;									-- Write Signal
-			ADDR			:	in  STD_LOGIC_VECTOR (N-1 downto 0);	-- Address to access data
-			READ_DATA	:	out STD_LOGIC_VECTOR (N-1 downto 0)		-- Data read from memory
+			W_ADDR		    :	in  STD_LOGIC_VECTOR (M-1 downto 0);	-- Address to write data
+			WRITE_DATA	    :	in  STD_LOGIC_VECTOR (N-1 downto 0);	-- Data to be written
+			MemWrite		:	in  STD_LOGIC;							-- Write Signal
+			ADDR			:	in  STD_LOGIC_VECTOR (M-1 downto 0);	-- Address to access data
+			READ_DATA	    :	out STD_LOGIC_VECTOR (N-1 downto 0)		-- Data read from memory
 		);
 	end component;
     
     
     -- the Registers block
     component register_file is
+        generic (
+            N: natural; -- RADDR_BUS
+            M: natural  -- DDATA_BUS
+        );
         port (
 			CLK 			:	in	STD_LOGIC;				
 			RESET			:	in	STD_LOGIC;				
 			RW				:	in	STD_LOGIC;				
-			RS_ADDR 		:	in	STD_LOGIC_VECTOR (RADDR_BUS-1 downto 0); 
-			RT_ADDR 		:	in	STD_LOGIC_VECTOR (RADDR_BUS-1 downto 0); 
-			RD_ADDR 		:	in	STD_LOGIC_VECTOR (RADDR_BUS-1 downto 0);
-			WRITE_DATA		:	in	STD_LOGIC_VECTOR (DDATA_BUS-1 downto 0); 
-			RS				:	out	STD_LOGIC_VECTOR (DDATA_BUS-1 downto 0);
-			RT				:	out	STD_LOGIC_VECTOR (DDATA_BUS-1 downto 0)
+			RS_ADDR 		:	in	STD_LOGIC_VECTOR (N-1 downto 0); 
+			RT_ADDR 		:	in	STD_LOGIC_VECTOR (N-1 downto 0); 
+			RD_ADDR 		:	in	STD_LOGIC_VECTOR (N-1 downto 0);
+			WRITE_DATA		:	in	STD_LOGIC_VECTOR (M-1 downto 0); 
+			RS				:	out	STD_LOGIC_VECTOR (M-1 downto 0);
+			RT				:	out	STD_LOGIC_VECTOR (M-1 downto 0)
         );
     end component;
             
@@ -123,7 +135,7 @@ begin
 	ALU1: alu generic map (N=>MEM_DATA_BUS)
 		-- the ALU between Registers and Data memory on the suggested architecture
 		port map (
-			X => ReadData1,
+			X => ,
 			Y => ReadData2,
 			R => ALU1_Result
 		);
